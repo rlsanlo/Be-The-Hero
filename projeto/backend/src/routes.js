dda0ad94b68095900cbb906b2d09d4b784f193ec
rlsanlo <rlsanlo@gmail.com>
2020-03-27 13:35:52 -0300

    Validação e testes no backend

--- projeto/backend/src/routes.js
+++ projeto/backend/src/routes.js
@@ -1,37 +1,58 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate')
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionsController = require('./controllers/SessionController');
 const routes = express.Router();
 
-/*
-* Metodos HTTP
-*
-* GET: Buscar uma informação do back-end
-* POST: Criar uma informação no back-end
-* PUT: Alterar uma informação no back-end
-* Delete: Deletar uma informação do back-end
-*/ 
-
-/*
-* Tipos de Parametro
-*
-* Query: Parametros nomeados enviados na rota após "?", servem para (filtros, paginação)
-* Routes Params: Parametros utilizados para identificar recursos
-* Request Body: corpo da requisição, utilizado para criar ou alterar recursos
-*/
-
-routes.post('/sessions', SessionsController.create);
+routes.post('/sessions', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required()
+    })
+}),SessionsController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
 
-routes.get('/profile', ProfileController.index);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}) ,OngController.create);
 
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/profile',celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown(),
+}) ,ProfileController.index);
 
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}),IncidentController.index);
+
+
+routes.post('/incidents', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number()
+    }),
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}),IncidentController.create);
+
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}),IncidentController.delete);
 
 module.exports = routes;
